d2d296326f9b91087c58156a4b0c1c964d76b06f
f0x1d <f0x1d@yandex-team.ru>
2025-06-06 00:53:02 +0300

    Initial Commit

---
+++ SwagCode/SwagCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwagCodeApp.swift
+//  SwagCode
+//
+//  Created by Maksim Zoteev on 06.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwagCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
